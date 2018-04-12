990cf068a908a40bebc6b2a591c22845a02229dc
Priyesh Kumar <priyeshkumar16@gmail.com>
2018-04-12 22:37:02 +0530

    first implementation of algorithm

--- Node.cpp
+++ Node.cpp
@@ -1,19 +1,22 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <queue>
 #include <string>
 #include <iostream>
+#include <fstream>
+
 using namespace std;
  
 struct Node {
-    public:
-        int ID;
-        bool USING;
-        bool ASKED;
-        Node* HOLDER;
-        queue<Node*> REQUEST_Q;
+
+    private:
+        /* private data for each node */
+        int ID;                         // unique id of node
+        bool USING;                     // true if using critical section
+        bool ASKED;                     // true is request is sent
+        Node* HOLDER;                   // next node to reach TOKEN
+        queue <Node*> REQUEST_Q;        // queue to store node requests
+        ofstream out_file, log_file;    // output and log files
  
+        /* Handles the use of TOKEN */
         void ASSIGN_PRIVILEGE() {
             if(HOLDER==this && !USING && !REQUEST_Q.empty()) {
                 HOLDER = REQUEST_Q.front();
@@ -22,36 +25,29 @@ struct Node {
                 if(HOLDER==this) {
                     USING = true;
                     // start executing its critical section
+                    log_file<<"Node "<<ID<<" has entered critical section"<<endl;
                     execute();
+                    HANDLE_EVENT("exit critical section", NULL);
                 }
                 else {
+                    log_file<<"Node "<<ID<<" passed the privilege to Node "<<HOLDER->ID<<endl;
                     HOLDER->HANDLE_EVENT("privilege", NULL);
                 }
             }
         }
- 
+         /* make request for the resource to the HOLDER */
         void MAKE_REQUEST() {
             if(HOLDER!=this && !REQUEST_Q.empty() && !ASKED) {
                 ASKED = true;
                 HOLDER->HANDLE_EVENT("request", this);
             }
         }
- 
+
+        /* Critical Section Code */
         void execute() {
-            // creating child process to handle CS
-            // pid_t pid = fork();
-            // if(pid==0){
-            //     /* child process */
-            //     // do CS;
-            //     // sleep(5000);
-            //     cout<<"I am (id): "<<ID<<endl;
-            //     HANDLE_EVENT("exit critical section", NULL);
-            //     execlp("/bin/ls","ls", NULL);
-            // }
-            // else{
-            //     wait(NULL);
-            // }
-        	cout<<ID<<" has been executed"<<endl;
+            out_file.open("output.txt", ofstream::app);
+            out_file<<"Node "<<ID<<" has executed"<<endl;
+            out_file.close();
         }
  
     public:
@@ -61,8 +57,10 @@ struct Node {
             USING = false;
             ASKED = false;
             HOLDER = current_dir;
+            log_file.open("log.txt", fstream::app);
         }
 
+        /* Message handling mechanism */
         void HANDLE_EVENT(string message, Node* sender) {
             if(message=="enter critical section") {
                 REQUEST_Q.push(this);
@@ -70,6 +68,7 @@ struct Node {
                 MAKE_REQUEST();
             }
             if(message=="request") {
+                log_file<<"Node "<<ID<<" received request from Node "<<sender->ID<<endl;
                 REQUEST_Q.push(sender);
                 ASSIGN_PRIVILEGE();
                 MAKE_REQUEST();
@@ -80,9 +79,11 @@ struct Node {
                 MAKE_REQUEST();
             }
             if(message=="exit critical section") {
+                log_file<<"Node "<<ID<<" exits critical section"<<endl;
                 USING = false;
                 ASSIGN_PRIVILEGE();
                 MAKE_REQUEST();
+                log_file.close();
             }
         }
 };
--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    // input validation
+    // input format validation
     if(argc!=3){
         printf("Usage: ./main <inputfile> nodeCount\n");
         return 0;
@@ -16,9 +16,12 @@ int main(int argc, char* argv[])
 
     // local variables
     int i;
+
     // creating Nodes
     int nodeCount = atoi(argv[2]);
     Node* Node_Array[nodeCount];
+
+    // create tree structure from nodes
     Node_Array[0] = new Node(0, NULL);
     Node_Array[0]->HANDLE_EVENT("privilege", NULL);
     for(i=1; i<nodeCount-1; i+=2){
@@ -28,11 +31,18 @@ int main(int argc, char* argv[])
     if((nodeCount&1)==0) Node_Array[i] = new Node(i, Node_Array[i/2]);
     
     //input processing from text file
-    ifstream file(argv[1]); // pass file name as argument
-    int linebuffer;
-    while(file>>linebuffer){
-        Node_Array[linebuffer]->HANDLE_EVENT("enter critical section", NULL);
-    }
-    // getchar();
+    ifstream file(argv[1]);             // pass file name as argument
+    ofstream log;                       // log file
+    log.open("log.txt", fstream::app);  // open log file in append mode
+    int linebuffer;                     // input integer node number
 
+    while(file>>linebuffer){
+        /* input checking and error handling*/
+        if(linebuffer >= 0 && linebuffer < nodeCount){
+
+            log<<"Node "<<linebuffer<<" wants to enter critical section"<<endl;
+                Node_Array[linebuffer]->HANDLE_EVENT("enter critical section", NULL);
+        }
+    }
+    log.close();
 }
